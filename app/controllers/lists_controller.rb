36e295e8a997ddc7e39887cd16227380c8943f43
Dari Goldman <darigoldman@gmail.com>
2022-10-22 12:50:55 +0200

    feat: added new and create actions in controller and views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,23 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     @movies = @list.movies
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create(list_params)
+    if @list.save
+      redirect_to lists_path, notice: 'List was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
