84ccf27dec3741860f05aede291a64d70d662902
Dari Goldman <darigoldman@gmail.com>
2022-10-25 21:29:00 +0200

    feat: Users can add images to list categories

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -23,6 +23,12 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to root_path
+  end
+
   private
 
   def list_params
